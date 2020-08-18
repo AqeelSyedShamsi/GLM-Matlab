a047312c1b460175eb2c8155718b4ac43b561a93
AqeelSyedShamsi <aqeelsyedshamsi@gmail.com>
2020-08-18 14:02:45 +0200

    Add files via upload

--- moreVoxelBOLDImpGLM.m
+++ moreVoxelBOLDImpGLM.m
@@ -1,6 +1,6 @@
 myfuns = moreVoxelBOLDstat;
-Mean =0; %Ground Truth mean
-
+pVal=[];
+pValue=[];
 DataModelallEffects=[];
 Data_star=[];
 stimDataX=[];
@@ -11,61 +11,70 @@ gaussianNoise=[];
 sigmaMat=[];
 modelDataX=[];
 
+k=[0,1,2,3,4,5,6,7];
+lVal=[0,1,2,3,4,5,6,7];
+Mean =0; %Ground Truth mean
 i = 300; %number of scans
 j = 1; %number of voxels
-
 % Creating a model of brain activity (BOLD)
-res = 2.4;%Stimulus RT in seconds
-%duration = 20; % seconds
-% create a canonical BOLD response
+res = 4;%Stimulus RT in seconds
+%create a canonical BOLD response
 TR=2;
 HR=80/60;
 BR=12/60;
 alpha = 0.05;
 loopItr = 10000;
-% betaMat=zeros (j,loopItr); %beta matrix
-%T=zeros (j,loopItr);       %T test matrix
-T=[];
-betaMat=[];
-%design matrix
-% c=zeros(1,j);
-% c(1)=1;
-%c=[1,0,0,0,0,0,0];
-%c=c';
-%creating gaussian signal and Design matrix
-gama=[0.001:0.001:0.008;0.002:0.002:0.016;0.01:0.01:0.08];
+
+
+gamma=[0.01:0.01:0.08];%;0.002:0.002:0.016;0.01:0.01:0.08];
 GMean=0;
-Gstd=[0.1,1,1.5];
+Gstd=[0.1];%,1,1.5];
 pVal=[];
+% for sni = 1:length(lVal)
+%     l = lVal(sni);
+for rhoi = 1:length(k)
+    l = lVal(rhoi);
+    T=[];
+    betaMat=[];
+for xx = 1:loopItr
 
-for gamaX = 1:length(gama(:,2))
-    gamma = gama(gamaX,:);
     
-    GStd  = Gstd(gamaX);
-    for xx = 1:loopItr
+    
+    [X,G,H, Gnoise, resp, stim, hrf] = myfuns.glm_simulate_data(i,j,TR,HR,BR, gamma,res,GMean, k, l, rhoi);% X,G should be gernarate at once before the loop
 
-    
-    
-        [X,G,H, Gnoise, resp, hrf] = myfuns.glm_simulate_data(i,j,TR,HR,BR, gamma,res,GMean, GStd);% X,G should be gernarate at once before the loop
-
-        %Parameters 
-        [b, beta, e, rss, r, sigma, inveG] = myfuns.glm_estimate(X,G);
-        %betaMat(jx, xx)= b;      
-        c=zeros(1,length(b));
-        c(1)=1;
-        c(2)=-1;
-        betaMat=[betaMat b];
-        t = myfuns.glm_inference (c, b);
-        %T (jx, xx)= t;
-        %T=[T (t(1,:))'];
-        T=[T t'];
+    %Parameters 
+    [b, beta, e, rss, r, sigma, inveG] = myfuns.glm_estimate(X,G);
+    %betaMat(jx, xx)= b;      
+    c=zeros(1,length(b));
+    c(1)=1;
+    c(2)=-1;
+    betaMat=[betaMat b];
         
-   
-    end
+    t = myfuns.glm_inference (c, sigma, b, G,i,rss,inveG);
+    %T (jx, xx)= t;
+    %T=[T (t(1,:))'];
+    T=[T t];
+    
+
+end
+    [X_mod, X_star, modelDataAllEffects] = myfuns.glm_model(G,e,b,H,gamma,X, Gnoise);
     p = 1-tcdf(T,i-1);
-    H1 = T < alpha;
+    H1 = p < alpha;
     aq=sum(H1,2);
     aq1=sum(aq);
-    disp(aq1/30000)
-    pVal=[pVal (aq1/30000)];
+    disp(aq1/loopItr)
+    pVal=[pVal (aq1/loopItr)];
+    
+    
+    DataModelallEffects = [DataModelallEffects modelDataAllEffects];
+    Data_star = [Data_star X_star];
+    stimDataX= [stimDataX X];
+    bVector=[bVector b];
+    designMatrix=[designMatrix G];
+    counfoundMatrix=[counfoundMatrix H];
+    gaussianNoise=[gaussianNoise Gnoise];
+    sigmaMat=[sigmaMat sigma];
+    modelDataX=[modelDataX X_mod];
 end
+pValue=[pValue pVal];
+% end
--- moreVoxelBOLDstat.m
+++ moreVoxelBOLDstat.m
@@ -6,18 +6,30 @@ function func = moreVoxelBOLDstat
     func.glm_model=@glm_model;
 
 end
-function [X, G, H, Gnoise, resp, hrf] = glm_simulate_data(i,j,TR,HR,BR, gamma,res,GMean,GStd)
+function [X, G, H, Gnoise, resp, stim, hrf] = glm_simulate_data(i,j,TR,HR,BR, gamma,res,GMean, k, l, rhoi)
+    G=ones(i,1); %design matrix
+    Gnoise = GMean + (0.5/(l+1)).*randn(i,1);%Gaussian Noise with mean 1 and standard deviation 2.
 
-    G=ones(i,1);%G = eye(i,j); %design matrix
-    Gnoise = GMean + GStd.*randn(i,1);%Gaussian Noise with mean 1 and standard deviation 2.
-
-    hrf = spm_hrf(1/res);
+    stim=zeros(i,1);
+%   %stim( 1*res:5*res:end ) =1;
+    stim( 10:30:end ) =1;
+    
+    hrf = spm_hrf(1/(res));
+    hrfCod2 = spm_hrf(1/(res+(k(rhoi)/10)));%k(rhoi)+1*(hrf);
+   
     resp=[];
+
     hrf1=hrf(1:30);
+    hrf2=hrfCod2(1:30);
+
     for respi = 1:i/60
         resp=vertcat(resp, hrf1);
+        resp=vertcat(resp,hrf2);
     end
-    
+    plot (resp)
+%     for respi2 = 1:i/60
+%         respCod2=vertcat(respCod2, hrf2);
+%     end
     
     %n1=1:1:i;
     %n=linspace (0,0.001,300) ;
@@ -28,33 +40,12 @@ function [X, G, H, Gnoise, resp, hrf] = glm_simulate_data(i,j,TR,HR,BR, gamma,re
 
     
     inputdata =rand(i,6);
-    H= (random('normal',0,GStd,size(inputdata))); %RANDOM data
+    H= (random('normal',0,(0.5/(l+1)),size(inputdata))); %RANDOM data
     H =[H hrFreq'];
     H =[H brFreq'];
     
-   
-    %resp= resp + Gnoise + (H*gamma');
-    %X= resp+ Gnoise + (H*gamma');
-    var1=vertcat(resp,zeros(length(resp),1));
-    var2=vertcat(zeros(length(resp),1),resp);
-    X=[(var1+Gnoise+(H*gamma')) (var2+Gnoise+(H*gamma'))];
-    fftCondA= fft(X(:,1));
-    fftCondB= fft(X(:,2));
-    
-    absFftCondA = abs(fftCondA);
-    absFftCondB = abs(fftCondB);
-
-    magFftCondA = max(absFftCondA);
-    magFftCondB = max(absFftCondB);
-    rho=magFftCondA / magFftCondB;
-
-    
-    %X=X+Gnoise;
-    G= [G X H];
-    
-    %X=[X ()];
-    
-    
+    G= [G resp H];
+    X= resp+ Gnoise + (H*gamma');
     
 end
 function [b, beta, e, rss, r, sigma, inveG] =glm_estimate(X,G)
@@ -70,8 +61,21 @@ function [b, beta, e, rss, r, sigma, inveG] =glm_estimate(X,G)
     sigma= sqrt(rss/r);
     
 end
-function [t] = glm_inference (c, b) %(c, beta, sigma, alpha)
+function [t,Cb] = glm_inference (c, sigma, b, G,i,rss,inveG) %(c, beta, sigma, alpha)
 
-    t=c*b/std(c*b) ;
+    Cb = (sigma^2)*(inveG);
+%     t = (c*b)/(((rss/(i-rank(G)))*(c*inveG*c'))^0.5);
+    t=c*b/std(c*b);
+end
+
+function [X_mod, X_star,modelDataAllEffects] =glm_model(G,e,b,H,gamma,X, Gnoise)
     
+    %%%Data model with all effects
+    modelDataAllEffects= G(:,[1,2])*b([1,2],1);% + e;
+    
+    %litH = H(:,[5,6,7,8]);
+    %litGamma=gamma(:,[5,6,7,8]);
+    X_mod= G(:,[1,2,9,10])*b([1,2,9,10],1);% + e
+    X_star= X-(H*(b(3:10,1))) - Gnoise;
+    %X_star = X-(H*((mean(H,1))')) - Gnoise ;
 end
